03403cb2cc4cd47d17bad0574b55afd3d2932926
Mary West <mwest.lazerade@gmail.com>
2021-04-18 16:32:41 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,44 @@ const enum DistanceUnit {
   //% block="inch"
   INCH = 148, // Duration of echo round-trip in Microseconds (uS) for two inches, 343 m/s at sea level and 20°C
 }
+/**
+ * Well known colors for a NeoPixel strip
+ */
+enum NeoPixelColors {
+    //% block=red
+    Red = 0xFF0000,
+    //% block=orange
+    Orange = 0xFFA500,
+    //% block=yellow
+    Yellow = 0xFFFF00,
+    //% block=green
+    Green = 0x00FF00,
+    //% block=blue
+    Blue = 0x0000FF,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=violet
+    Violet = 0x8a2be2,
+    //% block=purple
+    Purple = 0xFF00FF,
+    //% block=white
+    White = 0xFFFFFF,
+    //% block=black
+    Black = 0x000000
+}
+
+/**
+ * Different modes for RGB or RGB+W NeoPixel strips
+ */
+enum NeoPixelMode {
+    //% block="RGB (GRB format)"
+    RGB = 1,
+    //% block="RGB+W"
+    RGBW = 2,
+    //% block="RGB (RGB format)"
+    RGB_RGB = 3
+}
+
 
 /**
  * Functions to operate the CCS811
@@ -1215,10 +1253,10 @@ enum NeoPixelMode {
         return packRGB(r, g, b);
     }
 
-    export enum HueInterpolationDirection {
-        Clockwise,
-        CounterClockwise,
-        Shortest
+export enum HueInterpolationDirection {
+   Clockwise,
+   CounterClockwise,
+   Shortest
     }
 
   
